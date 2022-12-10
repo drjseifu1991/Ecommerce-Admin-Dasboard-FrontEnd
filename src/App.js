a6fbb8fbe4d02b74133e3fd9b074e0b81ff80c20
Dereje3030 <51210918+Dereje3030@users.noreply.github.com>
2022-12-10 20:51:10 +0300

    client finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
+import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import Layout from './scenes/layout'
+import Dashboard from './scenes/dashboard'
+import Products from './scenes/products'
+import Customers from './scenes/customers';
+import Transactions from './scenes/transactions';
+import Geography from './scenes/geography';
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path='/' element={<Navigate to='/dashboard' replace/>} />
+              <Route path='/dashboard' element={<Dashboard/>}/>
+              <Route path='/products' element={<Products/>}/>
+              <Route path='/customers' element={<Customers/>}/>
+              <Route path='/transactions' element={<Transactions/>}/>
+              <Route path='/geography' element={<Geography/>}/>
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
